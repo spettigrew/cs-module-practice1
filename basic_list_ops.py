6b3a7ceb1b19015cdd1c6aafb841cdf1e2db9973
Sara Pettigrew <sarapettigrew@MacBook-Pro.local>
2020-10-02 17:19:46 -0600

    Iterating strings

---
+++ basic_list_ops.py
@@ -0,0 +1,21 @@
+"""
+1. Create a "numbers" list that contains two different integer values and two different floating point values.
+
+2. Create a "strings" list that contains three different strings.
+
+3. Print the 4th number of your numbers list and the 1st string of your strings list.
+
+4. Iterate through your strings list and print each string.
+"""
+
+# YOUR CODE HERE
+
+numbers_list = [1, 2, 6.3, 8.9]
+
+strings_list = ["I", "like", "Python!"]
+
+print(numbers_list[3])
+print(strings_list[0])
+
+for element in strings_list:
+    print(element)
