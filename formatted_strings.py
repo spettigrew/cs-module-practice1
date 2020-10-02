f05b4d893a21f47b4992f3f32387ef36e9214e82
Sara Pettigrew <sarapettigrew@MacBook-Pro.local>
2020-10-02 17:21:32 -0600

    String formatted

---
+++ formatted_strings.py
@@ -0,0 +1,11 @@
+"""
+1. Assign three different types of data to the three variables "a", "b", and "c".
+
+2. Use a format string to inject the data from your three variables into the string.
+"""
+# Modify the code below to meet the requirements above.
+a = "Colette's Birthday!"
+b = 6
+c = 9.30
+
+print("Yay! It's %s Age: %d. Date: %.2f." % (a, b, c))
