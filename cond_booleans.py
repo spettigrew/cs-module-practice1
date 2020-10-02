fad200ea4135f9c66a3a57a0a290acc54026b760
Sara Pettigrew <sarapettigrew@MacBook-Pro.local>
2020-10-02 17:21:11 -0600

    Statements are true

---
+++ cond_booleans.py
@@ -0,0 +1,18 @@
+""""
+Change the assignment statements below so that all of the print function calls print True.
+"""
+# change these values so that all of the print statements print True
+number = 18
+second_number = 120
+first_list = [1, 2, 3]
+second_list = [1,2,]
+second_number = False
+# only change the code above this line
+
+# all of these print statements must print True by only changing the assignment statements above
+print(number > 15)
+print(first_list)
+print(len(second_list) == 2)
+print(len(first_list) + len(second_list) == 5)
+print(first_list and first_list[0] == 1)
+print(not second_number)
