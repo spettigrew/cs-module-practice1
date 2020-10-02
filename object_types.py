df0971ba891e292532ab205e29cf47e63d26941b
Sara Pettigrew <sarapettigrew@MacBook-Pro.local>
2020-10-02 17:21:58 -0600

    Typecasting on integars and float numbers

---
+++ object_types.py
@@ -0,0 +1,18 @@
+"""
+Modify the assignment statements below so that an integer is assigned to "my_int", a floating point is assigned to "my_float", and a string is assigned to "my_string".
+
+Then, use typecasting to print your integer as a floating point number. Use typecasting again to print your floating point number as an integer.
+"""
+
+# Modify the code below to fulfill the requirements outlined in the docstring.
+my_int = 3
+my_int = int(5.7)
+my_float = 2.0
+my_float = float(7)
+my_string = "hello"
+my_string = "Hello!"
+
+# Practice typecasting as outlined above with these print statements.
+print(my_int)
+print(my_float)
+print(my_string)
