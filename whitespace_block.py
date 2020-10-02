a238f4b928c7bd66c70593eaa22a50e7e506c368
Sara Pettigrew <sarapettigrew@MacBook-Pro.local>
2020-10-02 17:22:22 -0600

    Use of blocks and whitespace

---
+++ whitespace_block.py
@@ -0,0 +1,10 @@
+"""
+Modify the code below so that the print statement executes inside the block for the if statement.
+"""
+name = "Lambda School"
+
+
+if name == "Lambda School":
+
+
+    print("Unlocking potential, regardless of circumstance.")
