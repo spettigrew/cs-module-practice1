16bfd8c2d3ed3a12487ec365586d2a9ca855c029
Sara Pettigrew <sarapettigrew@MacBook-Pro.local>
2020-10-08 16:10:46 -0600

    User defined function setup

---
+++ user_defined_functions.py
@@ -0,0 +1,21 @@
+"""
+Modify this function to make it return the sum of the arguments a and b.
+"""
+def sum(a, b):
+    # DELETE THE PASS STATEMENT AND WRITE YOUR CODE HERE
+    return a + b
+
+# This should print 7
+print(sum(2, 5))
+
+
+"""
+Modify this function to use the sum function above to return
+the double of the sum of a and b.
+"""
+def double_the_sum(a, b):
+    # DELETE THE PASS STATEMENT AND WRITE YOUR CODE HERE
+    return (a + b) * 2
+
+# This should print 14
+print(double_the_sum(2, 5))
